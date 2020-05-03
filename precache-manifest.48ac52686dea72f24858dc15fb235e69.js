be4762ea5c316d1a6966d8f89c267a730d3d6300
MattInReality <47674538+MattInReality@users.noreply.github.com>
2020-05-03 22:02:59 +0100

    Updates

---
+++ precache-manifest.48ac52686dea72f24858dc15fb235e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b90022120c8f28411e1c87703ecc3b8",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "79ec872a3ab858965e5a",
+    "url": "/monsters_rolodex/static/css/main.96e9f862.chunk.css"
+  },
+  {
+    "revision": "757203268c401a5b9557",
+    "url": "/monsters_rolodex/static/js/2.1ef4fbdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.1ef4fbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ec872a3ab858965e5a",
+    "url": "/monsters_rolodex/static/js/main.5f2e82f8.chunk.js"
+  },
+  {
+    "revision": "a80e720810d0c06a54cb",
+    "url": "/monsters_rolodex/static/js/runtime-main.17c2740a.js"
+  }
+]);
